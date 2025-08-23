28caf900034808ed8a76ab751ecbbb3ed997f40a
brianwaithaka <waithakabrian8@gmail.com>
2025-08-23 06:18:24 +0300

    insert fetched data into desertcard

--- src/components/DessertCard.jsx
+++ src/components/DessertCard.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
-function DessertCard() {
+
+function DessertCard({ dessert }) {
+    const { image: { thumbnail, mobile, tablet, desktop }, name, category, price } = dessert
+    const formatter = new Intl.NumberFormat("en-us", { currency: "USD", style: "currency" })
     return (
         <div className='max-w-xs'>
-            <img src="/images/image-creme-brulee-mobile.jpg" alt="" />
+            <img src={mobile} alt={name} />
             <div>
-                <button>Add to Cart</button>
+                <button className='flex items-center gap-2 py-4 px-6 font-semibold border border-amber-700 rounded-full'><img src="/assets/images/icon-add-to-cart.svg" alt="add-to-cart-icon" />Add to Cart</button>
                 <div>
-                    <span>creme brulee</span>
-                    <span>vanilla bean creme brulee</span>
-                    <span>$6.50</span>
+                    <span>{category}</span>
+                    <span>{name}</span>
+                    <span>{formatter.format(price)}</span>
                 </div>
             </div>
         </div>
