fb4ffc35ee348c450b63c6e1a56bc4c052804392
brianwaithaka <waithakabrian8@gmail.com>
2025-08-23 05:48:28 +0300

    create context api

---
+++ src/contexts/DataContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const dessertsContext = createContext()
+
+
+
+function DessertsProvider({ children }) {
+    const [desserts, setDesserts] = useState()
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch("/data/data.json")
+                if (!response.ok) throw new Error("could not fetch desserts data")
+                const data = await response.json()
+                setDesserts(data)
+
+            } catch (error) {
+                console.error(error);
+
+            }
+        }
+        fetchData()
+    }, [])
+    return (
+        <dessertsContext.Provider value={{ desserts }}>{children}</dessertsContext.Provider>
+    )
+}
+
+function useDessertsApi() {
+    const context = useContext(dessertsContext)
+    if (context === undefined) throw new Error("dessertsContext was used outside of its provider")
+    return context
+}
+
+export { DessertsProvider, useDessertsApi }
