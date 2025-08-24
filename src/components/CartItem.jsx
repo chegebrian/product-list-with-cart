317226721af452788faedf41e7bbf01a1099e0d6
brianwaithaka <waithakabrian8@gmail.com>
2025-08-24 12:27:30 +0300

    calculate each item cost

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -3,12 +3,15 @@ import { useDessertsApi } from '../contexts/DataContext'
 
 function CartItem({ dessert }) {
     const { name, price } = dessert
-    const { setCart } = useDessertsApi()
+    const { setCart, cart } = useDessertsApi()
     function handleRemoveItem(selectedDessert) {
         setCart((cart) => cart?.filter((item) => item.name !== selectedDessert))
     }
+    const findDessert = cart?.find((item) => item.name === name)
+    console.log(findDessert.quantity);
+    
     const formatter = new Intl.NumberFormat("en-us", { style: "currency", currency: "USD" })
-    const numberOfDesserts = 2
+    const numberOfDesserts = findDessert.quantity
     const cost = price * numberOfDesserts
     return (
         <div className='py-4 flex items-center justify-between'>
@@ -17,7 +20,7 @@ function CartItem({ dessert }) {
                 <h3 className='text-amber-950 capitalize font-semibold text-lg'>{name}</h3>
                 <div className='flex items-center gap-4'>
 
-                    <span className='text-amber-600 font-semibold'>1x</span>
+                    <span className='text-amber-600 font-semibold'>{findDessert.quantity}x</span>
                     <span className='text-amber-900'>@{formatter.format(price)}</span>
                     <span className='text-amber-900 font-semibold'>{formatter.format(cost)}</span>
                 </div>
